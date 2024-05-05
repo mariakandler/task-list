13da922a93541b5ae6fe5e5f1428dc871d74997e
mariakandler <159329506+mariakandler@users.noreply.github.com>
2024-05-05 18:56:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const getStartedBtn = document.getElementById('getStartedBtn');
-const firstTaskSection = document.getElementById('firstTaskSection');
-const createTaskBtn = document.getElementById('createTaskBtn');
+const firstTaskAlert = document.getElementById('firstTaskAlert');
+const firstTaskBtn = document.getElementById('firstTaskBtn');
 const addTaskSection = document.getElementById('addTaskSection');
 const saveTaskBtn = document.getElementById('saveTaskBtn');
-const mainSection = document.getElementById('mainSection');
+const timeFramesSec = document.getElementById('timeFramesSec');
+const main = document.getElementById('main')
 
 
 // initial scroll
 getStartedBtn.addEventListener('click', () => 
-firstTaskSection.scrollIntoView({behavior: 'smooth'})
+main.scrollIntoView({behavior: 'smooth'})
 );
 
 // create task button
-createTaskBtn.addEventListener('click', () => {
-if (addTaskSection.classList.contains('hidden')) {
-    addTaskSection.classList.remove('hidden')
+firstTaskBtn.addEventListener('click', () => {
+if (FirstTaskInputsDiv.classList.contains('hidden')) {
+    FirstTaskInputsDiv.classList.remove('hidden');
+    firstTaskBtn.remove();
 } else {
-    addTaskSection.classList.add('hidden')
+    FirstTaskInputsDiv.classList.add('hidden')
 }
 });
 
 // adding task to html
-function addTaskHtml (title, desc, date){
+function addTaskHtml (title, desc, date, targetDiv){
     const taskContainer = document.createElement('div');
     const taskH1 = document.createElement('h1');
     const taskDesc = document.createElement('p');
@@ -33,63 +35,83 @@ function addTaskHtml (title, desc, date){
     taskDesc.innerHTML = desc;
     taskDate.innerHTML = date;
 
-    mainSection.appendChild(taskContainer);
     taskContainer.append(taskH1, taskDesc, taskDate);
+    targetDiv.append(taskContainer)
 }
 
 // validanting date
-
-function validateDate (date) {
-    const dueDateString = document.getElementById('dueDate').value;
-    const today = new Date().setHours(0, 0, 0, 0)
-    const beforeToday = 
-}
-
-// new task handler
-document.addEventListener('DOMContentLoaded', function() {
-saveTaskBtn.addEventListener('click', () => {
-    const taskTitle = document.getElementById('taskTitle').value;
-    const taskDescription = document.getElementById('taskDescription').value;
-    const dueDateString = document.getElementById('dueDate').value;
-    const dueDate = new Date(dueDateString + "T00:00:00");
-
-    if (mainSection.classList.contains('hidden')) {
-        mainSection.classList.remove('hidden');
-        addTaskHtml(taskTitle, taskDescription, dueDate);
-        getTimeFrame(dueDate)
-    } else {
-        addTaskHtml(taskTitle, taskDescription, dueDate);
-        getTimeFrame(dueDate)
-    }
-
-    document.getElementById('taskTitle').value = "";
-    document.getElementById('taskDescription').value = "";
-    document.getElementById('dueDate').value = "";
+function validateDate (dueDate) {
+    const today = new Date().setHours(0, 0, 0, 0);
     
-})
-})
+    if (dueDate < today) {
+        window.alert("data inválida");
+        return false;
+    } else {
+        return true;
+    }
+}
 
 // time frame handler
 function getTimeFrame (due) {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); 
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); 
+  
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0); 
+  
+    const dueDate = new Date(due);
+    dueDate.setHours(0, 0, 0, 0); 
 
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
-  tomorrow.setHours(0, 0, 0, 0); 
-
-  const dueDate = new Date(due);
-  dueDate.setHours(0, 0, 0, 0); 
-
-  const todayDiv = document.getElementById('todayDiv');
-  const tomorrowDiv = document.getElementById('tomorrowDiv');
-  const nextDiv = document.getElementById('nextDiv');
-
-  if (dueDate.getTime() === today.getTime()) {
-    console.log('today');
-  } else if (dueDate.getTime() === tomorrow.getTime()) {
-    console.log('tomorrow');
-  } else {
-    console.log('next');
+    const taskTitle = document.getElementById('taskTitle').value;
+    const taskDescription = document.getElementById('taskDescription').value;
+  
+    const todayDiv = document.getElementById('todayDiv');
+    const tomorrowDiv = document.getElementById('tomorrowDiv');
+    const nextDiv = document.getElementById('nextDiv');
+  
+    if (dueDate.getTime() === today.getTime()) {
+      addTaskHtml(taskTitle, taskDescription, dueDate, todayDiv);
+    } else if (dueDate.getTime() === tomorrow.getTime()) {
+      addTaskHtml(taskTitle, taskDescription, dueDate, tomorrowDiv);
+    } else {
+      addTaskHtml(taskTitle, taskDescription, dueDate, nextDiv);
+  } 
 }
+
+// clear inputs 
+function clearInputs () {
+    document.getElementById('taskTitle').value = "";
+    document.getElementById('taskDescription').value = "";
+    document.getElementById('dueDate').value = "";
 }
+
+// first task handler
+document.addEventListener('DOMContentLoaded', function() {
+    saveTaskBtn.addEventListener('click', () => {
+        
+        const dueDateString = document.getElementById('dueDate').value;
+        const dueDate = new Date(dueDateString + "T00:00:00");
+
+    
+        if (mainSection.classList.contains('hidden')) {
+            mainSection.classList.remove('hidden');
+            if (validateDate(dueDate)) {
+                getTimeFrame(dueDate);
+            } else {
+                window.alert('nao')
+            }
+        } else {
+            if (validateDate(dueDate)) {
+                getTimeFrame(dueDate);
+            } else {
+                window.alert('nao')
+            }
+        }
+
+    clearInputs();
+        
+    })
+})
+
+
